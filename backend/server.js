659d019555cc33e9c2577eb2f8cd697fdd99d11d
deepakramji <ramji.hariharan@gmail.com>
2023-11-21 20:30:07 +0530

    MongoDB Connection Setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import products from './data/products.js';
+import connectDB from './config/db.js';
 
-dotenv.config();
+
+
 const app = express();
+dotenv.config();
+connectDB();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
